b056075f2a385155234fd2b57d53e84cf00f2b9f
Henrique Yule <yzhenrique@gmail.com>
2025-08-24 11:26:40 -0400

    adicionando repositório de vacinas e integrando ao UnitOfWork

---
+++ src/CartaoVacina.Infrastructure/Repositories/VacinaRepository.cs
@@ -0,0 +1,31 @@
+using CartaoVacina.Application.Interfaces;
+using CartaoVacina.Domain.Entities;
+using CartaoVacina.Infrastructure.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace CartaoVacina.Infrastructure.Repositories;
+
+public class VacinaRepository : BaseRepository<Vacina>, IVacinaRepository
+{
+    public VacinaRepository(CartaoVacinaDbContext context) : base(context) { }
+
+    public async Task<Vacina?> GetByNomeAsync(string nome)
+    {
+        return await _context.Vacinas
+            .FirstOrDefaultAsync(v => v.Nome.ToLower() == nome.ToLower());
+    }
+
+    public override async Task<List<Vacina>> GetAllAsync()
+    {
+        return await _context.Vacinas.ToListAsync();
+    }
+
+    public void Delete(Guid id)
+    {
+        var vacina = _context.Vacinas.Find(id);
+        if (vacina != null)
+        {
+            _context.Vacinas.Remove(vacina);
+        }
+    }
+}
